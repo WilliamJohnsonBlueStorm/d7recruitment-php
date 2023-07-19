d8247fea3797265ce1b1ef30af69686e4cc77b6e
William Johnson <william.johnson@bluestormdesign.co.uk>
2023-07-19 09:32:02 +0100

    Converted homepage to php includes

---
+++ components/home-banner.php
@@ -0,0 +1,15 @@
+<section id="banner" aria-labelledby="main-title" class="mb-12 lg:mb-24">
+    <div class="container">
+        <div class="grid grid-cols-2 gap-8 items-center">
+            <div class="col-span-2 lg:col-span-1 order-2 lg:order-1">
+                <h1 id="main-title" class="text-h1 lg:text-h1-desktop text-brand-gold uppercase font-Novecento-sans font-bold mb-5">Health &amp; Safety Recruitment Specialists</h1>
+                <p class="text-brand-white text-lg mb-5 text-[24px]">A people focused recruitment solution placing experienced and competent professionals across the UK and Europe.</p>
+                <a href="#" title="latest jobs" aria-label="latest jobs" class="uppercase text-brand-blue text-center px-8 py-5 bg-brand-gold block md:inline-block mb-4 md:mb-0 font-bold border border-brand-gold rounded hover:bg-brand-darkgold transition-all duration-300">Latest Jobs <i class="fa fa-arrow-right ml-2" aria-hidden="true"></i></a>
+                <a href="#" title="latest jobs" aria-label="latest jobs" class="uppercase text-brand-white text-center px-8 py-5 block md:inline-block font-bold border border-brand-gold rounded md:ml-8 hover:bg-brand-gold hover:text-brand-blue transition-all duration-300">About Us <i class="fa fa-arrow-right ml-2" aria-hidden="true"></i></a>
+            </div>
+            <div class="col-span-2 lg:col-span-1 order-1 lg:order-2">
+                <img src="images/D7-header-1.jpeg" alt="Banner Image" class="rounded-l-full w-full h-auto">
+            </div>
+        </div>
+    </div>
+</section>
---
+++ components/home-callout-slider.php
@@ -0,0 +1,46 @@
+<div class="homepage-callouts-slider">
+    <!-- Slider main container -->
+    <div class="swiper">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            <a href="#" title="Your next role in health and safety" aria-label="Your next role in health and safety" class="swiper-slide block rounded overflow-hidden group h-auto">
+                <img src="/images/health-safety.png" alt="health and safety" class="h-[250px] lg:h-[301px]">
+
+                <div class="lg:p-5 group-hover:bg-brand-white h-full transition-all duration-300">
+                    <h3 class="text-h3 text-brand-gold font-Novecento-sans font-bold uppercase mb-3 group-hover:text-brand-darkgold transition-all duration-300">Your next role in health and safety</h3>
+                    <p class="text-brand-white group-hover:text-brand-darkblue">Are you looking for career progression? As health & safety and construction recruitment specialists, we have access to …</p>
+                </div>
+            </a>
+
+            <a href="#" title="Your next role in health and safety" aria-label="Your next role in health and safety" class="swiper-slide block rounded overflow-hidden group h-auto">
+                <img src="/images/work-with-us.png" alt="health and safety" class="h-[250px] lg:h-[301px]">
+
+                <div class="lg:p-5 group-hover:bg-brand-white h-full transition-all duration-300">
+                    <h3 class="text-h3 text-brand-gold font-Novecento-sans font-bold uppercase mb-3 group-hover:text-brand-darkgold transition-all duration-300">Looking for your next recruitment partner?</h3>
+                    <p class="text-brand-white group-hover:text-brand-darkblue">Founded by health and safety professionals, D7 understands first-hand the importance of placing the right person in the right role. Our recruiters are dedicated to securing you the best talent in the industry.</p>
+                </div>
+            </a>
+
+            <a href="#" title="Your next role in health and safety" aria-label="Your next role in health and safety" class="swiper-slide block rounded overflow-hidden group h-auto">
+                <img src="/images/Construction.jpeg" alt="health and safety" class="h-[250px] lg:h-[301px]">
+
+                <div class="lg:p-5 group-hover:bg-brand-white h-full transition-all duration-300">
+                    <h3 class="text-h3 text-brand-gold font-Novecento-sans font-bold uppercase mb-3 group-hover:text-brand-darkgold transition-all duration-300">Health & Safety Specialists</h3>
+                    <p class="text-brand-white group-hover:text-brand-darkblue">Our team will support in the recruitment of experienced and competent health and safety specialists for you on a permanent or fixed term contract basis.</p>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="hidden md:flex justify-between items-center xl:hidden">
+        <div class="swiper-pagination !static text-left !w-auto [&>.swiper-pagination-bullet]:w-[12px] [&>.swiper-pagination-bullet]:h-[12px] [&>.swiper-pagination-bullet-active]:bg-brand-white"></div>
+        <div class="flex">
+            <button class="home-swiper-button-prev !static text-[50px] text-brand-white mr-16 disabled:opacity-50 transition">
+                <i class="fa-solid fa-arrow-left"></i>
+            </button>
+            <button class="home-swiper-button-next !static text-[50px] text-brand-white disabled:opacity-50 transition">
+                <i class="fa-solid fa-arrow-right"></i>
+            </button>
+        </div>
+    </div>
+</div>
